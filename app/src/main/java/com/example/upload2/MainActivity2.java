aaa37911ddd149a6ea690e7b25045fde96bdc16d
an-hyewon <hyewon.an11@gmail.com>
2021-07-08 13:57:44 +0900

    commit

--- app/src/main/java/com/example/upload2/MainActivity2.java
+++ app/src/main/java/com/example/upload2/MainActivity2.java
@@ -16,6 +16,7 @@ import android.content.pm.ResolveInfo;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -30,11 +31,32 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
 import org.jetbrains.annotations.NotNull;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 import static android.Manifest.permission.CAMERA;
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
@@ -118,8 +140,16 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
             case R.id.select:
                 filePicker();
                 break;
-
             case R.id.upload:
+                if (mBitmap != null){
+                    for (int i = 0; i < mBitmap.size(); i++){
+                        multipartImageUpload(i);
+//                        FirebaseUpload(i);
+                    }
+                }
+                else {
+                    Toast.makeText(getBaseContext(), "Bitmap is null. Try again", Toast.LENGTH_SHORT).show();
+                }
                 break;
         }
     }
@@ -417,5 +447,170 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
         int column_index = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA);
         cursor.moveToFirst();
         return cursor.getString(column_index);
+    }
+
+    private void multipartImageUpload(int index) {
+        try {
+            String user_id = "abc";
+            String user_email = "abc@gmail.com";
+
+//            File filesDir = getApplicationContext().getFilesDir();
+//            File file = new File(filesDir, "image" + ".png");
+
+            File file = new File(imageListUri.get(index));
+//
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            mBitmap.get(index).compress(Bitmap.CompressFormat.PNG, 0, bos);
+            byte[] bitmapdata = bos.toByteArray();
+
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(bitmapdata);
+            fos.flush();
+            fos.close();
+
+
+            RequestBody reqFile = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+            MultipartBody.Part body = MultipartBody.Part.createFormData("file", file.getName(), reqFile);
+            RequestBody id = RequestBody.create(MediaType.parse("text/plain"), user_id);
+
+            Call<ResponseBody> req = serviceApi.postImage(body, id);
+            req.enqueue(new Callback<ResponseBody>() {
+                @Override
+                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                    if (response.code() == 200) {
+                        textView.setText("Uploaded Successfully!");
+                        textView.setTextColor(Color.BLUE);
+                    }
+                    Toast.makeText(getBaseContext(), response.code() + " ", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onFailure(Call<ResponseBody> call, Throwable t) {
+                    textView.setText("Uploaded Failed!");
+                    textView.setTextColor(Color.RED);
+                    Toast.makeText(getBaseContext(), "Request failed", Toast.LENGTH_SHORT).show();
+                    t.printStackTrace();
+                }
+            });
+
+//            Log.d("imageListUri", imageListUri.get(index));
+//            Log.d("realUri", String.valueOf(realUri.get(index)));
+//            Uri returnedUri = Uri.parse("file://"+imageListUri.get(index));
+//            InputStream inputStream = null;
+//            inputStream = getBaseContext().getContentResolver().openInputStream(realUri.get(index));
+//            mBitmap.set(index, BitmapFactory.decodeStream(inputStream));
+//            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+//            mBitmap.get(index).compress(Bitmap.CompressFormat.JPEG, 20, byteArrayOutputStream);
+
+
+//            RequestBody id = RequestBody.create(MediaType.parse("text/plain"), user_id);
+//            ArrayList<MultipartBody.Part> files = new ArrayList<>();
+//            for (int i =0; i<realUri.size(); ++i) {
+//                // Uri 타입의 파일경로를 가지는 RequestBody 객체 생성
+//                RequestBody fileBody = RequestBody.create(MediaType.parse("multipart/form-data"), byteArrayOutputStream.toByteArray());
+////                RequestBody fileBody = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+//                // RequestBody로 Multipart.Part 객체 생성
+//                MultipartBody.Part filePart = MultipartBody.Part.createFormData("file", file.getName(), fileBody);
+//                // 추가
+//                files.add(filePart);
+//            }
+//            Call<ResponseBody> requestfile = serviceApi.request(files, id);
+//            requestfile.enqueue(new Callback<ResponseBody>() {
+//                @Override
+//                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+//                    if (response.code() == 200) {
+//                        textView.setText("Uploaded Successfully!");
+//                        textView.setTextColor(Color.BLUE);
+//                    }
+//                    Toast.makeText(getBaseContext(), response.code() + " ", Toast.LENGTH_SHORT).show();
+//                }
+//
+//                @Override
+//                public void onFailure(Call<ResponseBody> call, Throwable t) {
+//                    textView.setText("Uploaded Failed!");
+//                    textView.setTextColor(Color.RED);
+//                    Toast.makeText(getBaseContext(), "Request failed", Toast.LENGTH_SHORT).show();
+//                    t.printStackTrace();
+//                }
+//            });
+
+//            RequestBody id = RequestBody.create(MediaType.parse("text/plain"), user_id);
+////            RequestBody email = RequestBody.create(MediaType.parse("text/plain"), user_email);
+//            map.put("id", id);
+////            map.put("email", email);
+//            File file2 = new File(imageListUri.get(index));
+//            InputStream inputStream = null;
+//            inputStream = getBaseContext().getContentResolver().openInputStream(realUri.get(index));
+//            mBitmap.set(index, BitmapFactory.decodeStream(inputStream));
+//            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+//            mBitmap.get(index).compress(Bitmap.CompressFormat.JPEG, 20, byteArrayOutputStream);
+//            RequestBody requestBody = RequestBody.create(MediaType.parse("image/jpg"), byteArrayOutputStream.toByteArray());
+//            MultipartBody.Part uploadFile = MultipartBody.Part.createFormData("postImg", file.getName() ,requestBody);
+//            serviceApi.postImages(uploadFile, map).enqueue(new Callback<ResponseBody>() {
+//                @Override
+//                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+//                    if (response.code() == 200) {
+//                        textView.setText("Uploaded Successfully!");
+//                        textView.setTextColor(Color.BLUE);
+//                    }
+//                    Toast.makeText(getBaseContext(), response.code() + " ", Toast.LENGTH_SHORT).show();
+//                }
+//
+//                @Override
+//                public void onFailure(Call<ResponseBody> call, Throwable t) {
+//                    textView.setText("Uploaded Failed!");
+//                    textView.setTextColor(Color.RED);
+//                    Toast.makeText(getBaseContext(), "Request failed", Toast.LENGTH_SHORT).show();
+//                    t.printStackTrace();
+//                }
+//            });
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void FirebaseUpload(int index) {
+        //firebase storage에 업로드하기
+        //1. FirebaseStorage을 관리하는 객체 얻어오기
+        FirebaseStorage firebaseStorage= FirebaseStorage.getInstance();
+
+        //2. 업로드할 파일의 node를 참조하는 객체
+        //파일 명이 중복되지 않도록 날짜를 이용
+        SimpleDateFormat sdf= new SimpleDateFormat("yyyyMMddhhmmss");
+        String filename= sdf.format(new Date())+ ".png";//현재 시간으로 파일명 지정 20191023142634
+        //원래 확장자는 파일의 실제 확장자를 얻어와서 사용해야함. 그러려면 이미지의 절대 주소를 구해야함.
+
+        StorageReference imgRef= firebaseStorage.getReference("uploads/"+filename);
+        //uploads라는 폴더가 없으면 자동 생성
+
+        //참조 객체를 통해 이미지 파일 업로드
+        // imgRef.putFile(imgUri);
+        //업로드 결과를 받고 싶다면..
+        try {
+            if (tempUri == null){
+                imgRef.putFile(photoUri);
+                Log.i("photoUri","upload 성공");
+            } else{
+                InputStream stream = new FileInputStream(String.valueOf(realUri.get(index)));
+                UploadTask uploadTask =imgRef.putStream(stream);
+//            UploadTask uploadTask =imgRef.putFile(Uri.parse(imageListUri.get(index)));
+                uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Log.i(String.valueOf(index),"upload 성공");
+                        Toast.makeText(getBaseContext(), String.valueOf(index)+": success upload", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        //업로드한 파일의 경로를 firebaseDB에 저장하면 게시판 같은 앱도 구현할 수 있음.
+
     }
 }
